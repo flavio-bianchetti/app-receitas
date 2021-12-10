7f317d6b0a942967d70f4da3c4e5a4fb24644e12
matt-pessoa <matheuspessoafonseca@gmail.com>
2021-12-10 15:47:50 -0300

    G1 - Adiciona imagens aos links e fixa a posiçao do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './css/Footer.css';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
   return (
     <footer data-testid="footer">
       <Link to="/bebidas">
         <img
-          src="../images/drinkIcon.svg"
+          src={ drinkIcon }
           alt="Go to drinks"
           data-testid="drinks-bottom-btn"
         />
       </Link>
       <Link to="/explorar">
         <img
-          src="../images/exploreIcon.svg"
+          src={ exploreIcon }
           alt="Explore"
           data-testid="explore-bottom-btn"
         />
       </Link>
       <Link to="/comidas">
         <img
-          src="../images/mealsIcon.svg"
+          src={ mealIcon }
           alt="Go to meals"
           data-testid="food-bottom-btn"
         />
