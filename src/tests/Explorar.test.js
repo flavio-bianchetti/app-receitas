cac2677d8e25091c66cbab0d6610440383967037
José Erivam <erivamdev@gmail.com>
2022-01-16 11:48:28 -0300

    testando se tem o botao e o texto do botao

--- src/tests/Explorar.test.js
+++ src/tests/Explorar.test.js
@@ -10,11 +10,15 @@ beforeEach(() => {
 describe('Verifica os Botões do componente Explorar', () => {
   it('Verifica se tem o Botão de Explorar Comidas', () => {
     const buttonFoods = screen.getByTestId('explore-food');
+    expect(buttonFoods).toBeInTheDocument();
     expect(buttonFoods).not.toBeDisabled();
+    expect(buttonFoods).toHaveTextContent('Explorar Comidas');
   });
 
   it('Verifica se tem o Botão Explorar Bebidas', () => {
     const buttonDrinks = screen.getByTestId('explore-drinks');
+    expect(buttonDrinks).toBeInTheDocument();
     expect(buttonDrinks).not.toBeDisabled();
+    expect(buttonDrinks).toBeInTheDocument('Explorar Bebidas');
   });
 });
