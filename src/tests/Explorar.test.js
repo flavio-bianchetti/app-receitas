4cf3be31ee06425013271178b3d63331f1a9d9ef
José Erivam <erivamdev@gmail.com>
2022-01-16 11:57:03 -0300

    teste do componente Explorar

--- src/tests/Explorar.test.js
+++ src/tests/Explorar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouterAndProvider from '../renderWithRouterAndProvider';
 
@@ -8,17 +8,23 @@ beforeEach(() => {
 });
 
 describe('Verifica os Botões do componente Explorar', () => {
-  it('Verifica se tem o Botão de Explorar Comidas', () => {
+  it('Verifica Botão de Explorar Comidas', async () => {
     const buttonFoods = screen.getByTestId('explore-food');
     expect(buttonFoods).toBeInTheDocument();
-    expect(buttonFoods).not.toBeDisabled();
     expect(buttonFoods).toHaveTextContent('Explorar Comidas');
+    expect(buttonFoods).not.toBeDisabled();
+
+    fireEvent.click(buttonFoods);
+    await screen.findByText('Explorar Comidas');
   });
 
-  it('Verifica se tem o Botão Explorar Bebidas', () => {
+  it('Verifica se tem o Botão Explorar Bebidas', async () => {
     const buttonDrinks = screen.getByTestId('explore-drinks');
     expect(buttonDrinks).toBeInTheDocument();
-    expect(buttonDrinks).not.toBeDisabled();
     expect(buttonDrinks).toBeInTheDocument('Explorar Bebidas');
+    expect(buttonDrinks).not.toBeDisabled();
+
+    fireEvent.click(buttonDrinks);
+    await screen.findByText('Explorar Bebidas');
   });
 });
